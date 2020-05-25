ec4394b2d8d969274bb224bdc34111eb2c0ee685
Jorge Ossa <jorgehossa@gmail.com>
2020-05-25 01:41:35 -0500

    Create package.json and install some dependecies
    Install: Parcel-bundler and Swiper.js
    
    Finish all sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,30 @@
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
+
+// Mobile Menu
 const $menuTrigger = document.querySelector('.nav__menu-icon');
-
-
 $menuTrigger.onclick = (e) => e.currentTarget.parentElement.classList.toggle('active');
 
-// $menuTrigger.addEventListener('click', (e) => e.currentTarget.parentElement.classList.toggle('active'));
+// Swiper.js
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  initialSlide: 0,
+  centeredSlides: true,
+  // init: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    380: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+      mousewheel: true,
+      initialSlide: 1,
+      grabCursor:true,
+      centeredSlides: true,
+    },
+  }
+});
